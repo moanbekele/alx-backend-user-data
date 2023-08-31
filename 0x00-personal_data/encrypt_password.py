5a01e7ec2f37694faf05d46aadc2863849b0621b
MoanBekele <moanbekele@gmail.com>
2023-09-01 00:08:59 +0300

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""Password Incription.
+"""A module for encrypting passwords.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hash password by using a random salt.
+    """Hashes a password using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks the integrity of the hashed password by comparing it to the given password.
+    """Checks is a hashed password was formed from the given password.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
